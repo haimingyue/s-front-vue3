4a26d0e40d57e2eb04cba636900d7e58c260e8e0
Simoon <420526391@qq.com>
2024-09-17 09:05:35 +0800

    felx介绍

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
+  <div class="flex items-center justify-start bg-black bg-opacity-30">
+    <img src="/100*100.png" class="w-22 h-full" alt="" />
     <Menu></Menu>
-    <router-view></router-view>
-    <div>footer</div>
   </div>
+  <router-view></router-view>
+  <div>footer</div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style lang="scss" scoped></style>
