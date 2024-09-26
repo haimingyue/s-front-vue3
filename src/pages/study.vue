cb733564af8077d034fe8f65f7069546fcab4e84
simoon <420526391@qq.com>
2024-09-26 10:01:50 +0800

    解决嵌套路由多个footer的问题

--- src/pages/study.vue
+++ src/pages/study.vue
@@ -1,26 +1,35 @@
 <template>
-  <div>
+  <div class="flex-1 flex flex-col">
     <!-- 头部内容 -->
     <div class="relative">
-      <Swiper :items="items" :height="36 * store.rate + 'rem'"></Swiper>
-      <div class="absolute bottom-0 left-0 w-full flex py-2 bg-gray-900 bg-opacity-40 z-20">
+      <!-- Swiper -->
+      <Swiper :items="items" :height="36 * store.rate + 'rem'" :pagination_hide="true"></Swiper>
+      <!-- Swiper里面登录 -->
+      <div
+        class="absolute bottom-0 left-0 w-full flex py-2 bg-gray-900 bg-opacity-40 z-20 lt-sm:display-none"
+      >
         <Container>
           <div class="flex justify-between w-full">
-            <ul class="flex types text-white">
+            <!-- 分类 -->
+            <ul class="flex types text-white" v-if="$route.name === '/study/cart'">
+              <li class="active item">购物车</li>
+            </ul>
+            <ul class="flex types text-white" v-else>
               <li class="active item">每日一课</li>
               <li class="item">精品微课</li>
               <li class="item">学习计划</li>
               <li class="item">优质专栏</li>
             </ul>
             <div class="flex items-center">
-              <div class="relative bg-white rounded-6 pr-2 pl-4 h-full flex items-center mr-4">
-                <input class="outline-none mr-2" type="text" placeholder="搜索课程" />
-                <i class="i-ep-search"></i>
+              <!-- 搜索 -->
+              <div class="relative bg-white rounded-6 pl-4 pr-2 h-full flex items-center mr-4">
+                <input type="text" class="outline-none mr-2" placeholder="搜索课程" />
+                <i class="i-ep:search"></i>
               </div>
-              <div class="flex text-gray-50">
-                <div class="mr-2 cursor-pointer hover:text-white">登录</div>
-                |
-                <div class="ml-2 cursor-pointer hover:text-white">注册</div>
+              <!-- 登录 -->
+              <div class="text-gray-200">
+                <span class="mr-2 cursor-pointer hover:text-white">登录</span>|
+                <span class="ml-2 cursor-pointer hover:text-white">注册</span>
               </div>
             </div>
           </div>
@@ -32,58 +41,52 @@
 </template>
 
 <script setup lang="ts">
-import { useThemeStore } from '../store/useThemeStore'
 import type { SwiperItemType } from '@/components/types'
-const store = useThemeStore()
+
+import { useThemeStore } from '@/store/useThemeStore'
+
 import bg from '@/assets/images/bg.png'
+
+const store = useThemeStore()
+
 const items: SwiperItemType[] = [
   {
     image: bg,
-    title: '传播技术的种子1',
+    title: '传播技术的种子',
     subTitle: '让技术没有门槛，让沟通没有障碍',
-    url: 'https://www.baidu.com'
+    url: 'https://www.imooc.com'
+  },
+  {
+    image: bg,
+    title: '传播技术的种子1',
+    subTitle: '让技术没有门槛，让沟通没有障碍1',
+    url: 'https://www.imooc.com'
   },
   {
     image: bg,
     title: '传播技术的种子2',
-    subTitle: '让技术没有门槛，让沟通没有障碍'
+    subTitle: '让技术没有门槛，让沟通没有障碍2',
+    url: 'https://www.imooc.com'
   },
   {
     image: bg,
     title: '传播技术的种子3',
-    subTitle: '让技术没有门槛，让沟通没有障碍'
-  },
-  {
-    image: bg,
-    title: '传播技术的种子4',
-    subTitle: '让技术没有门槛，让沟通没有障碍'
+    subTitle: '让技术没有门槛，让沟通没有障碍3',
+    url: 'https://www.imooc.com'
   }
 ]
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
 .types {
   .item {
-    @apply px-4 py-1 rounded-6 bg-gray-100 bg-opacity-40 mr-2 cursor-pointer;
+    @apply px-4 py-1 rounded-6 bg-gray-100 bg-opacity-20 mr-2 cursor-pointer;
     &:hover {
-      @apply bg-white text-dark-300;
+      @apply bg-white text-dark-200;
     }
   }
   .active {
     @apply bg-white text-dark-300;
   }
 }
-.tabs {
-  .active {
-    @apply text-sky-400 relative;
-    &::after {
-      @apply absolute bg-sky-400 w-8 h-[2px] bottom-0 left-1/2 transform-translate-x--1/2;
-      content: ' ';
-    }
-  }
-}
 </style>
-<route lang="yaml">
-meta:
-  layout: default
-</route>
--- src/pages/study/index.vue
+++ src/pages/study/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
+  <div class="pb-15">
     <!-- 推荐内容 -->
     <Container>
-      <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-full lt-sm:px-4">
+      <div class="text-xl py-2 lg:(text-3xl pb-2 pt-6) font-bold">推荐内容</div>
+    </Container>
+    <Container>
+      <div class="grid grid-cols-2 sm:grid-cols-4 gap-3 w-full lt-sm:px-4 py-4">
         <a
           :href="item.url"
           target="_blank"
@@ -18,29 +21,205 @@
             border
           >
             <template #default>
-              <div class="mx-4 flex items-center justify-between text-gray-500 mb-4">
-                <span>Simoon</span>|
-                <span>前端</span>
+              <div
+                class="px-2 py-1 lg:(px-4 mb-4) flex items-center justify-start text-gray-500 text-xs lg:text-base"
+              >
+                <span class="mr-2">Brian</span>|
+                <span class="ml-2">前端课程</span>
               </div>
-              <div class="mx-4 flex items-center justify-between text-gray-500 mb-4">
-                <span>￥3900</span>
-                <span>2000人学习</span>
-                <div class="i-mdi:cart"></div>
+              <div
+                class="px-2 py-1 lg:(px-4 mb-4) flex items-center justify-between text-gray-500 mb-4 text-xs lg:text-base"
+              >
+                <div>
+                  <span class="mr-2 text-orange">￥{{ item.price }}</span>
+                  <span>{{ item.counts }}人学习</span>
+                </div>
+                <div class="i-mdi:cart lg:text-xl xl:text-3xl text-sky-400"></div>
               </div>
             </template>
           </Card>
         </a>
       </div>
     </Container>
+    <Container>
+      <div class="text-xl py-2 lg:(text-3xl pb-2 pt-6) font-bold">每日一课</div>
+    </Container>
     <!-- 每日一课 -->
+    <Container class="flex-col">
+      <!-- tabs -->
+      <Tabs :items="['Java', 'Vue', 'React', '小程序']" v-model="activeIndex"></Tabs>
+      <!-- Grid -->
+      <div
+        class="grid grid-cols-2 py-4 lt-sm:px-4 sm:grid-cols-3 lg:(grid-cols-6 grid-rows-2 ) gap-3"
+      >
+        <!-- 左侧 row*2 col*2 -->
+        <div class="lg:(col-start-1 col-span-2 row-start-1 row-span-2)">
+          <a
+            :href="item.url"
+            target="_blank"
+            v-for="(item, index) in [recommend[0]]"
+            :key="index"
+            class="flex flex-col h-full"
+          >
+            <Card
+              class="w-full rounded-3 flex-1 h-full flex flex-col pb-4 justify-between transition-all hover:(transform-translate-y--1 shadow-lg)"
+              :image="item.image"
+              :height="'lg:(h-65)'"
+              image-type="rounded"
+              :title="item.title"
+              :clamp="2"
+              border
+            >
+              <template #default>
+                <div class="px-2 sm:px-4 flex items-center justify-start text-gray-500">
+                  <span class="mr-2">Brian</span>|
+                  <span class="ml-2">前端课程</span>
+                </div>
+              </template>
+            </Card>
+          </a>
+        </div>
+        <!-- 右侧 8 * item -> row*2 col*4 -->
+        <div v-for="(item, index) in [...recommend, ...recommend]" :key="index">
+          <a :href="item.url" target="_blank" class="flex">
+            <Card
+              class="w-full rounded-3 transition-all hover:(transform-translate-y--1 shadow-lg)"
+              height="h-30"
+              :image="item.image"
+              image-type="rounded"
+              :title="item.title"
+              title-class="py-1 px-4 text-lg"
+              :clamp="2"
+              border
+            >
+              <template #default>
+                <div class="mx-4 flex items-center justify-start text-gray-500 text-sm mb-4">
+                  <span class="mr-2">Brian</span>|
+                  <span class="ml-2">前端课程</span>
+                </div>
+              </template>
+            </Card>
+          </a>
+        </div>
+      </div>
+      <!-- <div class="flex">
+        <div class="w-1/3">1</div>
+        <div class="w-2/3">
+          <div class="grid grid-cols-4 grid-rows-2 gap-3">2</div>
+        </div>
+      </div> -->
+    </Container>
+    <Container>
+      <div class="text-xl py-2 lg:(text-3xl pb-2 pt-6) lg:text-3xl font-bold">精品微课</div>
+    </Container>
     <!-- 精品微课 -->
+    <Container class="overflow-x-auto">
+      <div class="grid grid-cols-[repeat(4,1fr)] gap-3 w-full lt-sm:px-4 py-4">
+        <a
+          :href="item.url"
+          target="_blank"
+          v-for="(item, index) in [...recommend, ...recommend]"
+          :key="index"
+          class="flex min-w-60"
+        >
+          <Card
+            class="w-full rounded-3 transition-all hover:(transform-translate-y--1 shadow-lg)"
+            :image="item.image"
+            image-type="rounded"
+            :title="item.title"
+            border
+          >
+            <template #default>
+              <div
+                class="px-2 py-1 lg:(px-4 mb-4) flex items-center justify-start text-gray-500 text-xs lg:text-base"
+              >
+                <span class="mr-2">Brian</span>|
+                <span class="ml-2">前端课程</span>
+              </div>
+              <div
+                class="px-2 py-1 lg:(px-4 mb-4) flex items-center justify-between text-gray-500 mb-4 text-xs lg:text-base"
+              >
+                <div>
+                  <span class="mr-2 text-orange">￥{{ item.price }}</span>
+                  <span>{{ item.counts }}人学习</span>
+                </div>
+                <div class="i-mdi:cart text-base lg:text-xl xl:text-3xl text-sky-400"></div>
+              </div>
+            </template>
+          </Card>
+        </a>
+      </div>
+    </Container>
     <!-- 学习计划 -->
+    <Container>
+      <div class="text-xl py-2 lg:(text-3xl pb-2 pt-6) font-bold">学习计划</div>
+    </Container>
+    <Container class="lt-lg:overflow-x-auto py-4">
+      <div class="grid grid-cols-[repeat(4,1fr)] gap-3">
+        <div
+          class="lt-sm:px-4 py-4 relative h-40 flex items-center justify-center transition-all cursor-pointer hover:(transform-translate-y--1 shadow-lg) group min-w-60"
+          v-for="(item, index) in recommend"
+          :key="index"
+        >
+          <div
+            class="absolute w-full h-full bg-cover bg-center rounded-3 overflow-hidden"
+            :style="{ 'background-image': 'url(' + item.image + ')' }"
+          >
+            <div class="bg-black bg-opacity-30 w-full h-full group-hover:display-none"></div>
+          </div>
+          <div class="z-30 px-4 line-clamp-3 text-base lg:text-xl text-white">{{ item.title }}</div>
+        </div>
+      </div>
+    </Container>
+    <Container>
+      <div class="text-xl py-2 lg:(text-3xl pb-2 pt-6) font-bold">优质专栏</div>
+    </Container>
     <!-- 优质专栏 -->
+    <Container class="overflow-x-auto">
+      <div class="grid grid-cols-[repeat(4,1fr)] gap-3 w-full lt-sm:px-4 py-4">
+        <a
+          :href="item.url"
+          target="_blank"
+          v-for="(item, index) in [...recommend, ...recommend]"
+          :key="index"
+          class="flex min-w-60"
+        >
+          <Card
+            class="w-full rounded-3 transition-all hover:(transform-translate-y--1 shadow-lg)"
+            :image="item.image"
+            image-type="rounded"
+            :title="item.title"
+            border
+          >
+            <template #default>
+              <div
+                class="px-2 py-1 lg:(px-4 mb-4) flex items-center justify-start text-gray-500 text-xs lg:text-base"
+              >
+                <span class="mr-2">Brian</span>|
+                <span class="ml-2">前端课程</span>
+              </div>
+              <div
+                class="px-2 py-1 lg:(px-4 mb-4) flex items-center justify-between text-gray-500 mb-4 text-xs lg:text-base"
+              >
+                <div>
+                  <span class="mr-2 text-orange">￥{{ item.price }}</span>
+                  <span>{{ item.counts }}人学习</span>
+                </div>
+                <div class="i-mdi:cart text-base lg:text-xl xl:text-3xl text-sky-400"></div>
+              </div>
+            </template>
+          </Card>
+        </a>
+      </div>
+    </Container>
   </div>
 </template>
 
 <script setup lang="ts">
 import bg from '@/assets/images/bg.png'
+
+const activeIndex = ref(0)
+
 const recommend = ref([
   {
     image: bg,
